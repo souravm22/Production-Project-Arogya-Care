86cebaf960d60f3fb2f1dea58a4cf8330c8c6641
souravm22 <sourav321m@gmail.com>
2024-06-05 12:10:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ app.use(moragan("dev"));
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+// static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 // port
 
 const port = process.env.PORT || 8080;
